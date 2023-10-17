3fdb695d38c833b226b33c8d34ee2641dd994cb2
sahilyadav2023 <143372003+sahilyadav2023@users.noreply.github.com>
2023-10-17 20:38:52 +0530

    Update Q6 l4.py

--- Q6 l4.py
+++ Q6 l4.py
@@ -1,10 +1,11 @@
 N=int(input("Enter any number: "))
+temp=N
 a=0
 while N>0:
     r=N%10
-    N=N//10
     a=(10*a)+r
-if a==N:
+    N=N//10
+if a==temp:
         print("It is Palindrome ")
 else:
     print("It is not palindrome ")
